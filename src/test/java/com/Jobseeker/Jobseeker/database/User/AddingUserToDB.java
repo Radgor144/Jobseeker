09af049004b7a51d26501fa5acfdc9588a32f4b7
Radek <radek.goralik@interia.pl>
2024-08-22 20:17:02 +0200

    Added AddingUserToDB test

---
+++ src/test/java/com/Jobseeker/Jobseeker/database/User/AddingUserToDB.java
@@ -0,0 +1,37 @@
+package com.Jobseeker.Jobseeker.database.User;
+
+import com.Jobseeker.Jobseeker.dataBase.Repositories.UserRepository;
+import com.Jobseeker.Jobseeker.dataBase.User.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class AddingUserToDB {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Test
+    void shouldAddUserToDB() {
+        User user = new User();
+        user.setFirstName("John");
+        user.setLastName("Snow");
+        user.setEmail("John@gmail.com");
+        user.setPassword("password");
+
+        User savedUser = userRepository.save(user);
+
+        assertThat(entityManager.find(User.class, savedUser.getId()) ).isEqualTo(user);
+    }
+}
